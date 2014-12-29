3c7a1c2327193d4f1318f99ac86aac656cd293d3
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-12-29 05:12:56 +0000

    used fetchPage method

--- ampersand-infinite-scroll.js
+++ ampersand-infinite-scroll.js
@@ -5,21 +5,17 @@ var _ = require('underscore');
 function InfiniteScrollClosure(options){	
 	var def = {
 		reverse: false,
-		gap: 20,
-		fetch: {
-			limit: 10
-		}
+		gap: 20
 	};
 	options || (options = {});
 	var reverse = (options.reverse || def.reverse);
 	var gap = (options.gap || def.gap);
-	var fetch = (options.fetch || def.fetch);
 
 	_.extend(this, {events: {scroll: 'infiniteScroll'}}); 
 
 	this.infiniteScroll = function cb(){
 		if(this.el.scrollHeight - this.el.scrollTop <= this.el.clientHeight + gap){
-			this.collection({data: fetch});
+			this.collection.fetchPage();
 		}
 	};
 }
