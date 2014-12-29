f5e606a8054a88a92d3cc282c520ef4b64ba0660
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-12-29 03:31:54 +0000

    working version

--- ampersand-infinite-scroll.js
+++ ampersand-infinite-scroll.js
@@ -1,23 +1,24 @@
 /*$AMPERSAND_VERSION*/
 var View = require('ampersand-view');
+var _ = require('underscore');
 
 function InfiniteScrollClosure(options){	
 	var def = {
 		reverse: false,
-		threshold: 20,
+		gap: 20,
 		fetch: {
 			limit: 10
 		}
 	};
 	options || (options = {});
 	var reverse = (options.reverse || def.reverse);
-	var threshold = (options.threshold || def.threshold);
+	var gap = (options.gap || def.gap);
 	var fetch = (options.fetch || def.fetch);
 
-	this.events.scroll = 'infiniteScroll';
+	_.extend(this, {events: {scroll: 'infiniteScroll'}}); 
 
 	this.infiniteScroll = function cb(){
-		if(this.el.scrollHeight - this.el.scrollTop >= this.el.clientHeight - threshold){
+		if(this.el.scrollHeight - this.el.scrollTop <= this.el.clientHeight + gap){
 			this.collection({data: fetch});
 		}
 	};
@@ -26,8 +27,9 @@ function InfiniteScrollClosure(options){
 var InfiniteScrollView;
 
 InfiniteScrollView = function(options){
-	InfiniteScrollClosure.call(this, options.infiniteScroll);
+	options || (options = {});
 	BaseView.call(this, options);
+	InfiniteScrollClosure.call(this, options);
 };
 
 var BaseView = View.extend();
